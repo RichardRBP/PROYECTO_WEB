f11382893ff96a87b93187263808dd508e6156eb
Richard Braul <richardzz@hotmail.com>
2022-08-08 00:06:55 -0500

    Project created

---
+++ src/test/java/com/ingenieriaweb/springboot/app/ProyectoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieriaweb.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
