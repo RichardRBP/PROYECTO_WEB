a895c3d514ed468eebf8a34b01e44cf43e16002c
leodan12 <leomacip4@gmail.com>
2022-08-17 12:03:47 -0500

    prueba reporte empleado

--- src/main/java/com/ingenieriaweb/springboot/app/controllers/EmpleadoController.java
+++ src/main/java/com/ingenieriaweb/springboot/app/controllers/EmpleadoController.java
@@ -29,6 +29,24 @@ import com.ingenieriaweb.springboot.app.paginator.PageRender;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 import java.net.MalformedURLException;
+///////////////
+//importaciones para los reportes
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/empleado")
@@ -40,6 +58,64 @@ public class EmpleadoController {
 
     @Autowired
     private IUploadFileService uploadFileService;
+    /////////////////////
+    
+    //metodos para los reportes en jasper reports
+    final Logger log = LoggerFactory.getLogger(this.getClass());
+    final ModelAndView model = new ModelAndView();
+    
+    
+ // Method to display the index page of the application.
+    @GetMapping(value= "/welcome")
+    public ModelAndView index() {
+         log.info("Showing the welcome page.");
+         model.setViewName("welcome");
+        return model;
+    }
+    
+ // Method to create the pdf report via jasper framework.
+    @GetMapping(value = "/view")
+    public ModelAndView viewReport() {
+        log.info("Preparing the pdf report via jasper.");
+        try {
+            createPdfReport(EmpleadoService.findAll());
+            log.info("File successfully saved at the given path.");
+        } catch (final Exception e) {
+            log.error("Some error has occurred while preparing the employee pdf report.");
+            e.printStackTrace();
+        }
+        // Returning the view name as the index page for ease.
+        model.setViewName("welcome");
+        return model;
+    }
+ 
+    // Method to create the pdf file using the employee list datasource.
+    private void createPdfReport(final List<Empleado> empleados) throws JRException {
+        // Fetching the .jrxml file from the resources folder.
+        final InputStream stream = this.getClass().getResourceAsStream("/report.jrxml");
+ 
+        // Compile the Jasper report from .jrxml to .japser
+        final JasperReport report = JasperCompileManager.compileReport(stream);
+ 
+        // Fetching the employees from the data source.
+        final JRBeanCollectionDataSource source = new JRBeanCollectionDataSource(empleados);
+ 
+        // Adding the additional parameters to the pdf.
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Grupo 8 Ing Web-TPF");
+ 
+        // Filling the report with the employee data and additional parameters information.
+        final JasperPrint print = JasperFillManager.fillReport(report, parameters, source);
+ 
+        // Users can change as per their project requrirements or can take it as request input requirement.
+        // For simplicity, this tutorial will automatically place the file under the "c:" drive.
+        // If users want to download the pdf file on the browser, then they need to use the "Content-Disposition" technique.
+        final String filePath = "d://";
+        // Export the report to a PDF file.
+        JasperExportManager.exportReportToPdfFile(print, filePath + "Empleado_reporte.pdf");
+    }
+    ////////////////////////////////////////////////////
+    //metodo del empleado
 
     @GetMapping(value = "/uploads/{filename:.+}")
     public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
