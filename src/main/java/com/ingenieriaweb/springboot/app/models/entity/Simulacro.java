6d3a12523580813a23ea0516b24307a04b936b71
Leandro Villarroel <lvillarroel@unitru.edu.pe>
2022-08-30 13:01:22 -0500

    Parte Leandro

---
+++ src/main/java/com/ingenieriaweb/springboot/app/models/entity/Simulacro.java
@@ -0,0 +1,78 @@
+package com.ingenieriaweb.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "simulacros")
+public class Simulacro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String nombre;
+    
+    @NotEmpty
+    private Date Fecha;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "simulacro", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Matricula_Simulacro> matriculas_simulacro;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFecha() {
+		return Fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		Fecha = fecha;
+	}
+
+	public List<Matricula_Simulacro> getMatriculas_simulacro() {
+		return matriculas_simulacro;
+	}
+
+	public void setMatriculas_simulacro(List<Matricula_Simulacro> matriculas_simulacro) {
+		this.matriculas_simulacro = matriculas_simulacro;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+    
+    
+
+    
+}
